c52ca0c450a1a5f9b797fb45835d7ceb07e5cc7c
rakRandom <112525075+neoRandom@users.noreply.github.com>
2025-04-14 04:14:28 -0300

    refactor(User): add documentId field

--- app/src/main/java/com/example/appfirebase/data/model/User.kt
+++ app/src/main/java/com/example/appfirebase/data/model/User.kt
@@ -1,8 +1,12 @@
 package com.example.appfirebase.data.model
 
+import com.google.firebase.firestore.DocumentId
 import kotlin.ranges.contains
 
 data class User(
+    @DocumentId
+    val documentId: String = "",
+
     var firstName: String = "",
     var lastName: String = "",
     var email: String = "",
